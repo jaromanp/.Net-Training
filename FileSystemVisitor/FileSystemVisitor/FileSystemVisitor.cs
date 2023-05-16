7e4095a1b9401846236ca3c106d9b7782a19d280
jaromanp <alejandrojrp10@gmail.com>
2023-05-16 15:11:14 -0500

    First Draf, implementing class and filters

---
+++ FileSystemVisitor/FileSystemVisitor/FileSystemVisitor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystemVisitor
+{
+    public class FileSystemVisitor
+    {
+        private readonly Func<string, bool> _filter;
+        private readonly string _root;
+
+        public FileSystemVisitor(string root, Func<string, bool> filter = null)
+        {
+            _root = root;
+            _filter = filter ?? (s => true);
+        }
+
+        public IEnumerable<string> GetFileSystemEntries()
+        {
+            return GetFileSystemEntries(_root);
+        }
+
+        private IEnumerable<string> GetFileSystemEntries(string path)
+        {
+            foreach (var entry in Directory.GetFileSystemEntries(path))
+            {
+                if (_filter(entry))
+                {
+                    yield return entry;
+                }
+
+                if (Directory.Exists(entry))
+                {
+                    foreach (var subEntry in GetFileSystemEntries(entry))
+                    {
+                        yield return subEntry;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ FileSystemVisitor/FileSystemVisitor/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FileSystemVisitor
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var visitor = new FileSystemVisitor(@"C:\Repos\C-sharp-Interfaces-and-generics-3152729-main");
+            foreach (var entry in visitor.GetFileSystemEntries())
+            {
+                 Console.WriteLine((File.GetAttributes(entry) & FileAttributes.Directory) == FileAttributes.Directory ? "- " + entry : "* " + entry);
+            }
+
+            var visitor2 = new FileSystemVisitor(@"C:\", s => s.EndsWith(".txt"));
+            foreach (var entry in visitor.GetFileSystemEntries())
+            {
+                Console.WriteLine(entry);
+            }
+
+            var visitor3 = new FileSystemVisitor(@"C:\", s => new FileInfo(s).Length > 1024 * 1024);
+            foreach (var entry in visitor.GetFileSystemEntries())
+            {
+                Console.WriteLine(entry);
+            }
+
+            var visitor4 = new FileSystemVisitor(@"C:\", s => new FileInfo(s).CreationTime > DateTime.Now.AddDays(-7));
+            foreach (var entry in visitor.GetFileSystemEntries())
+            {
+                Console.WriteLine(entry);
+            }
+        }
+    }
+}
