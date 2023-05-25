db279de28e5ac57fdd4f746654cd94a42dbb8d2c
jaromanp <alejandrojrp10@gmail.com>
2023-05-24 20:28:15 -0500

    Task 1

---
+++ Exception Handling/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Enter a string: ");
+                    string input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        throw new Exception("Input cannot be empty");
+                    }
+
+                    if (input.ToLower() == "exit")
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine(input[0]);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
