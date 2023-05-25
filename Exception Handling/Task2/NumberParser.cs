e9f183764d66a95521998170863da68809dcb074
jaromanp <alejandrojrp10@gmail.com>
2023-05-24 21:52:32 -0500

    Task 2 completed

--- Exception Handling/Task2/NumberParser.cs
+++ Exception Handling/Task2/NumberParser.cs
@@ -6,7 +6,51 @@ namespace Task2
     {
         public int Parse(string stringValue)
         {
-            throw new NotImplementedException();
+            if (stringValue is null)
+            {
+                throw new ArgumentNullException("Input cannot be null");
+            } 
+
+            if (string.IsNullOrWhiteSpace(stringValue)) {
+                throw new FormatException("Input contains non-numeric characters");
+            }
+
+            if (stringValue == "-2147483648")
+            { return int.MinValue; }
+
+            if(stringValue == "2147483647")
+            { return int.MaxValue; }
+
+            stringValue = stringValue.Trim();
+
+            int result = 0;
+            int sign = 1;
+            int i = 0;
+
+            if (stringValue[0] == '-')
+            {
+                sign = -1;
+                i++;
+            }
+            else if (stringValue[0] == '+')
+            {
+                i++;
+            }
+
+            for (; i < stringValue.Length; i++)
+            {
+                if (stringValue[i] < '0' || stringValue[i] > '9')
+                {
+                    throw new FormatException("Input contains non-numeric characters");
+                }
+
+                checked
+                {
+                    result = result * 10 + (stringValue[i] - '0');
+                }
+            }
+
+            return sign * result;
         }
     }
 }
