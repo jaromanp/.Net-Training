25916bb82d8de072cd8a6856382dd1ba08ba3084
jaromanp <alejandrojrp10@gmail.com>
2023-06-07 14:57:52 -0500

    Task 1 completed

--- CustomAttributeDemo/Program.cs
+++ CustomAttributeDemo/Program.cs
@@ -31,17 +31,35 @@ namespace CustomAttributeDemo
 
             //// Modify settings
             Console.WriteLine("Now lets modify the settings and save it to the app.config");
-            mySettings.MyStringSetting = "New value";
-            mySettings.MyIntSetting = 123;
-            mySettings.MyTimeSpanSetting = TimeSpan.FromMinutes(30);
+            Console.WriteLine("New value for MyStringSetting");
+            mySettings.MyStringSetting = Console.ReadLine();
+            Console.WriteLine("New value for MyIntSetting");
+            try
+            {
+                mySettings.MyIntSetting = int.Parse(Console.ReadLine());
+            } catch (InvalidCastException ex)
+            {
+                Console.WriteLine($"{ex.Message}");
+            }
+            Console.WriteLine("New value for MyTimeSpanSetting in Minutes");
+            try
+            {
+                mySettings.MyTimeSpanSetting = TimeSpan.FromMinutes(int.Parse(Console.ReadLine()));
+            }
+            catch (InvalidCastException ex)
+            {
+
+                Console.WriteLine($"{ex.Message}");
+            }
 
             mySettings.SaveSettings();
 
-            string appConfigFilePath = "../../../app.config";
-            string appConfigContent = File.ReadAllText(appConfigFilePath);
+            Console.WriteLine("Let's load the settings again to see if it was succesfully");
+            mySettings.LoadSettings();
 
-            Console.WriteLine("New app.config content:");
-            Console.WriteLine(appConfigContent);
+            Console.WriteLine("My string setting is: {0}", mySettings.MyStringSetting);
+            Console.WriteLine("My int setting is: {0}", mySettings.MyIntSetting);
+            Console.WriteLine("My TimeSpanSetting setting is: {0}", mySettings.MyTimeSpanSetting);            
         }
     }
 
