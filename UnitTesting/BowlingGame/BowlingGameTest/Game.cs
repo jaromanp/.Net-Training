1ba68de561794b03b5576d96094ec4612cf41b4d
jaromanp <alejandrojrp10@gmail.com>
2023-06-24 11:29:46 -0500

    First iteration to handle a strike

--- UnitTesting/BowlingGame/BowlingGameTest/Game.cs
+++ UnitTesting/BowlingGame/BowlingGameTest/Game.cs
@@ -20,7 +20,12 @@
             int frameIndex = 0;
             for (int frame = 0; frame < 10; frame++)
             {
-                if (isSpare(frameIndex)) 
+                if (_rolls[frameIndex] == 10) //This would be a strike
+                {
+                    score += 10 + _rolls[frameIndex + 1] + _rolls[frameIndex + 2];
+                    frameIndex++;
+                }
+                else if (isSpare(frameIndex))
                 {
                     score += 10 + _rolls[frameIndex + 2];
                     frameIndex += 2;
