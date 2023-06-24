968c89ace92f9aa4ff7a6a9fddf136b4c6cafc1a
jaromanp <alejandrojrp10@gmail.com>
2023-06-24 10:49:51 -0500

    Test for spare working

--- UnitTesting/BowlingGame/BowlingGameTest/Game.cs
+++ UnitTesting/BowlingGame/BowlingGameTest/Game.cs
@@ -2,7 +2,6 @@
 {
     public class Game
     {
-        private int _score = 0;
         private int[] _rolls = new int[21];
         private int _currentRoll = 0;
 
@@ -12,20 +11,27 @@
 
         public void roll(int pins)
         {
-            _score += pins;
-            _rolls[++_currentRoll] = pins;
+            _rolls[_currentRoll++] = pins;
         }
 
         public int score()
         {
             int score = 0;
-            int i = 0;
-            for (int frame = 0; frame < 10; frame++) 
-            { 
-                score += _rolls[i] + _rolls[i+1];
-                i += 2;
+            int frameIndex = 0;
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (_rolls[frameIndex] + _rolls[frameIndex + 1] == 10) //This would be a spare
+                {
+                    score += 10 + _rolls[frameIndex + 2];
+                    frameIndex += 2;
+                }
+                else
+                {
+                    score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                    frameIndex += 2;
+                }
             }
             return score;
-        }
+        }        
     }
 }
