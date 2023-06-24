ecdb6c8f079ba4b1418aacc74bbec67cbddfcd05
jaromanp <alejandrojrp10@gmail.com>
2023-06-24 11:17:50 -0500

    Adding test for a Strike

--- UnitTesting/BowlingGame/BowlingGameTest/BowlingGameTest.cs
+++ UnitTesting/BowlingGame/BowlingGameTest/BowlingGameTest.cs
@@ -31,20 +31,31 @@ namespace BowlingGameTest
             Assert.That(g.score(), Is.EqualTo(0));
         }
 
-        [TestCase(20, 1)]
-        public void AllShootsOnePinReturnNumberOfShootsAsScore(int shoots, int pins)
+        [TestCase(20, 1, 20)]
+        public void AllShootsOnePinReturnNumberOfShootsAsScore(int shoots, int pins, int expectedScore)
         {
             rollMany(shoots, pins);
-            Assert.That(g.score(), Is.EqualTo(20));
+            Assert.That(g.score(), Is.EqualTo(expectedScore));
         }
 
-        [Test]
-        public void OneSpareShouldReturnTheExtraScore()
+        [TestCase(17, 0, 16)]
+        public void OneSpareShouldReturnTheExtraScore(int shoots, int pins, int expectedScore)
         {
             rollSpare();
             g.roll(3);
-            rollMany(17, 0);
-            Assert.That(g.score(), Is.EqualTo(16));
+            rollMany(shoots, pins);
+            Assert.That(g.score(), Is.EqualTo(expectedScore));
         }
+
+        [TestCase(16, 0, 24)]
+        public void OneStrikeReturnCorrectScore(int shoots, int pins, int expectedScore)
+        {
+            g.roll(10);
+            g.roll(3);
+            g.roll(4);
+            rollMany(shoots, pins);
+            Assert.That(g.score(), Is.EqualTo(expectedScore));
+        }
+
     }
 }
