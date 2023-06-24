e225448c82d5e14008348bfa6902d33b8090691f
jaromanp <alejandrojrp10@gmail.com>
2023-06-23 22:23:48 -0500

    Implementing all rolls test

--- UnitTesting/BowlingGame/BowlingGameTest/BowlingGameTest.cs
+++ UnitTesting/BowlingGame/BowlingGameTest/BowlingGameTest.cs
@@ -2,20 +2,32 @@ namespace BowlingGameTest
 {
     public class Tests
     {
+        private Game g;
+
         [SetUp]
         public void Setup()
         {
+            g = new Game();
         }
 
         [Test]
-        public void testGutterGame()
-        {
-            Game g = new Game();
+        public void testInitialGame()
+        {            
             for (int i = 0; i < 20; i++)
             {
                 g.roll(0);
             }
             Assert.That(g.score(), Is.EqualTo(0));
+        }
+
+        [Test]
+        public void testAllRolls()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                g.roll(1);
+            }
+            Assert.That(g.score(), Is.EqualTo(20));
         }
     }
 }
