8a95794ebafe158da69f063260f615d3585f4778
jaromanp <alejandrojrp10@gmail.com>
2023-06-24 10:34:57 -0500

    Second iteration for score method still failing test for spare

--- UnitTesting/BowlingGame/BowlingGameTest/Game.cs
+++ UnitTesting/BowlingGame/BowlingGameTest/Game.cs
@@ -3,6 +3,9 @@
     public class Game
     {
         private int _score = 0;
+        private int[] _rolls = new int[21];
+        private int _currentRoll = 0;
+
         public Game()
         {
         }
@@ -10,11 +13,19 @@
         public void roll(int pins)
         {
             _score += pins;
+            _rolls[++_currentRoll] = pins;
         }
 
         public int score()
         {
-            return _score;
+            int score = 0;
+            int i = 0;
+            for (int frame = 0; frame < 10; frame++) 
+            { 
+                score += _rolls[i] + _rolls[i+1];
+                i += 2;
+            }
+            return score;
         }
     }
 }
