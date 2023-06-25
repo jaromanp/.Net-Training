621a24b8da06eb58c8820360ab9ced796ec01745
jaromanp <alejandrojrp10@gmail.com>
2023-06-25 17:15:28 -0500

    Adding a test for the GetDiscount method

--- UnitTesting/HarryPotter/HarryPotterTest/ShoppingCartTest.cs
+++ UnitTesting/HarryPotter/HarryPotterTest/ShoppingCartTest.cs
@@ -92,9 +92,53 @@ namespace HarryPotterLibrary
             };
 
             var potterBooks = new ShoppingCart(books);
-            var price = potterBooks.GetPrice();
+            double price = potterBooks.GetPrice();
 
-            Assert.That(price, Is.EqualTo((16 * (1 - 0.05) + 8)));
+            double expected = (16 * (1 - 0.05)) + 8;
+
+            Assert.That(price, Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void BuyingThreeDifferentBooks()
+        {
+            var books = new Dictionary<BookType, int>
+            {
+                {BookType.PhilosophersStone, 1},
+                {BookType.ChamberOfSecrets, 1},
+                {BookType.PrisonerOfAzkaban, 1},
+                {BookType.GobletOfFire, 0},
+                {BookType.OrderOfPhoenix, 0},
+            };
+
+            var potterBooks = new ShoppingCart(books);
+            double price = potterBooks.GetPrice();
+
+            double expected = 24 * (1 - 0.1);
+
+            Assert.That(price, Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 0)]
+        [TestCase(2, 0.05)]
+        [TestCase(3, 0.1)]
+        [TestCase(4, 0.2)]
+        [TestCase(5, 0.25)]
+        public void ReturningAllDiscountsForDifferentTypesOfBooks(int nTypes, double expectedDiscount)
+        {
+            var books = new Dictionary<BookType, int>
+            {
+                {BookType.PhilosophersStone, 0},
+                {BookType.ChamberOfSecrets, 0},
+                {BookType.PrisonerOfAzkaban, 0},
+                {BookType.GobletOfFire, 0},
+                {BookType.OrderOfPhoenix, 0},
+            };
+
+            var potterBooks = new ShoppingCart(books);
+            var discount = potterBooks.GetDiscount(nTypes);
+
+            Assert.That(discount, Is.EqualTo(expectedDiscount));
         }
     }
 }
