e59030f45049c156d9fc55d754906513fcd0223f
jaromanp <alejandrojrp10@gmail.com>
2023-06-29 16:35:25 -0500

    Task 2 completed

---
+++ SQLFundamentals/SQLFundamentalsProject/dbo/Views/EmployeeInfo.sql
@@ -0,0 +1,10 @@
+﻿CREATE VIEW [dbo].[EmployeeInfo]
+	AS 
+		SELECT e.Id AS 'EmployeeId',
+		ISNULL(e.EmployeeName, CONCAT(p.FirstName,' ',  p.LastName)) AS 'EmployeeFullName',
+		CONCAT(a.ZipCode,'_', a.State, ',', a.City,'-', a.Street) AS 'EmployeeFullAddress',
+		CONCAT(e.CompanyName, '(', e.Position, ')') AS 'EmployeeCompanyInfo'
+		FROM dbo.Employee AS e
+		JOIN dbo.Person AS p ON p.Id = e.PersonId
+		JOIN dbo.Address AS a ON a.Id = e.AddressId
+		
