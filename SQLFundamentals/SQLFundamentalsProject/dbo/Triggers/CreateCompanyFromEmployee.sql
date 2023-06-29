03b07984717350bbdb53626a4032cbc86eff0571
jaromanp <alejandrojrp10@gmail.com>
2023-06-29 18:20:51 -0500

    Task 4 completed

---
+++ SQLFundamentals/SQLFundamentalsProject/dbo/Triggers/CreateCompanyFromEmployee.sql
@@ -0,0 +1,11 @@
+﻿CREATE TRIGGER [CreateCompanyFromEmployee]
+	ON dbo.Employee
+AFTER INSERT
+AS
+SET NOCOUNT ON;
+BEGIN
+    INSERT INTO dbo.Company (Name, AddressId)
+    SELECT CompanyName, AddressId
+    FROM inserted
+END
+
