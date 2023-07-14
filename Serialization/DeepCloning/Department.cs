3aee7c002b2b52bc9a5b201700fb6d06267d9f58
jaromanp <alejandrojrp10@gmail.com>
2023-07-13 22:06:44 -0500

    Task 3 finished

---
+++ Serialization/DeepCloning/Department.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeepCloning
+{
+    [Serializable]
+    public class Department : ICloneable
+    {
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+
+        public Department() { }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("Department Name: {0}\n", Name);
+            sb.AppendLine("Employees:");
+            foreach (Employee employee in Employees)
+            {
+                sb.AppendFormat("\t{0}\n", employee.EmployeeName);
+            }
+            return sb.ToString();
+        }
+
+        public object Clone()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                if (this.GetType().IsSerializable)
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(stream, this);
+                    stream.Position = 0;
+                    return formatter.Deserialize(stream);
+                }
+                return null;
+            }
+        }
+    }
+}
---
+++ Serialization/DeepCloning/Program.cs
@@ -0,0 +1,34 @@
+﻿using DeepCloning;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Department department = new Department()
+        {
+            Name = "Engineering",
+            Employees = new List<Employee>()
+            {
+                new Employee() { EmployeeName = "Alex" },
+                new Employee() { EmployeeName = "Maria" },
+                new Employee() { EmployeeName = "Albert" }
+            }
+        };
+
+        Department departmentClone = department.Clone() as Department;
+
+        Console.WriteLine("Original Department Object: ");
+        Console.WriteLine(department.ToString());
+
+        department.Name = "Testing";
+        department.Employees.RemoveAt(2);
+        department.Employees[1].EmployeeName = "Oscar";
+
+        Console.WriteLine("Original modified Department Object: ");
+        Console.WriteLine(department.ToString());
+        Console.WriteLine("Cloned department Object: ");
+        Console.WriteLine(departmentClone.ToString());
+    }
+}
