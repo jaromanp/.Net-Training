9bf5d9f75b9b8371b3a66815c2d47eca622c28cb
jaromanp <alejandrojrp10@gmail.com>
2023-07-13 21:38:11 -0500

    Task 2 completed

---
+++ Serialization/Task2/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    [Serializable]
+    public class Pet : ISerializable
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Pet() { }
+
+        public override string ToString()
+        {            
+            return String.Format("My Pet Name is {0} and it's {1} years old", Name, Age);
+        }
+
+        protected Pet(SerializationInfo info, StreamingContext context)
+        {
+            Name = (string)info.GetValue("Name", typeof(string));
+            Age = (int)info.GetValue("Age", typeof(int));
+        }
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Name", Name);
+            info.AddValue("Age", Age);
+        }
+    }
+}
---
+++ Serialization/Task2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using Task2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pet kitty = new Pet() { Name = "Snowbell", Age = 8};
+       
+        Console.WriteLine("Original object:");
+        Console.WriteLine(kitty.ToString());
+
+        Console.WriteLine("Now let's serialize the object into a file");
+        MemoryStream stream = new MemoryStream();
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(stream, kitty);
+
+        Console.WriteLine("Clear the kitty object");
+        kitty = null;
+
+        Console.WriteLine("Deserialize object:");
+        stream.Seek(0, SeekOrigin.Begin);
+        kitty = (Pet)formatter.Deserialize(stream);
+
+        Console.WriteLine(kitty.ToString());
+    }
+}
